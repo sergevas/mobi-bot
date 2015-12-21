b7dbc118632119662035feba13be0e016c331ed9
Sergey Vasiliev <brighterr@gmail.com>
2015-12-22 02:02:52 +0300

    Development in progress.

--- bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/route/sensor/HcSr505Route.java
+++ bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/route/sensor/HcSr505Route.java
@@ -1,6 +1,7 @@
 package xyz.sergevas.iot.bcbot.robot.route.sensor;
 
 import static xyz.sergevas.iot.bcbot.robot.common.IConstants.GPIO_PIN_HCSR505_PIN_LEFT;
+import static xyz.sergevas.iot.bcbot.robot.common.IConstants.GPIO_PIN_HCSR505_PIN_RIGHT;
 
 import org.apache.camel.builder.RouteBuilder;
 
@@ -8,7 +9,10 @@ public class HcSr505Route extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("pi4j-gpio://" + GPIO_PIN_HCSR505_PIN_LEFT + "?mode=DIGITAL_INPUT&state=LOW")
+		from("pi4j-gpio://" + GPIO_PIN_HCSR505_PIN_LEFT + "?mode=DIGITAL_INPUT&state=HIGH")
+		  .to("log:xyz.sergevas.iot.bcbot?level=DEBUG&showHeaders=true");
+		
+		from("pi4j-gpio://" + GPIO_PIN_HCSR505_PIN_RIGHT + "?mode=DIGITAL_INPUT&state=HIGH")
 		  .to("log:xyz.sergevas.iot.bcbot?level=DEBUG&showHeaders=true");
 	}
 }
