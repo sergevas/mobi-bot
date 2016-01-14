7d3642e6faa38089969409cd76eb47422fc27870
Sergey Vasiliev <brighterr@gmail.com>
2016-01-14 03:41:10 -0800

    Development in progress.

--- bcbot/bcbot-coap-gateway/src/main/java/xyz/sergevas/iot/bcbot/coap/gateway/route/GatewayRoute.java
+++ bcbot/bcbot-coap-gateway/src/main/java/xyz/sergevas/iot/bcbot/coap/gateway/route/GatewayRoute.java
@@ -6,10 +6,18 @@ public class GatewayRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("coap:localhost:5683/robot/sensor/distance/front")
+        restConfiguration("coap").host("localhost").port(5683);
+		rest("/robot/sensor/distance/front")
+		    .get()
+		    .to("direct:robotSensorDistanceFront");
+		
+		from("direct:robotSensorDistanceFront")
+		    .process(exchange -> exchange.getOut().setBody("30"));
+		
+		/*from("coap:localhost:5683/robot/sensor/distance/front")
 		    .convertBodyTo(String.class)
 		    .to("log:exch")
-		    .transform(body().prepend(30))
-		    .to("log:exch");
+		    .transform(body().prepend("30"))
+		    .to("log:exch");*/
 	}
 }
