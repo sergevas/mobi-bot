a6a8bfd7939f1f3aec212554806758a6115c0299
Sergey Vasiliev <brighterr@gmail.com>
2016-02-15 02:47:03 +0300

    The robot software development in progress.

--- bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/common/RobotMain.java
+++ bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/common/RobotMain.java
@@ -1,8 +1,11 @@
 package xyz.sergevas.iot.bcbot.robot.common;
 
+import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.main.Main;
 import org.apache.log4j.Logger;
+//import org.eclipse.californium.core.CoapServer;
 
+import xyz.sergevas.iot.bcbot.robot.route.sensor.CoAPMockRoute;
 import xyz.sergevas.iot.bcbot.robot.route.sensor.HcSr505Route;
 
 public class RobotMain {
@@ -11,9 +14,18 @@ public class RobotMain {
 
 	public static void main(String[] args) {
 		LOG.debug("Robot awakening...");
+//		CoapServer coapServer = new CoapServer();
+//		coapServer.add(resources);
+		
+		PropertiesComponent propertiesComponent = new PropertiesComponent();
+		propertiesComponent.setLocation("classpath:bcbot.properties");
+		
 		Main main = new Main();
+		main.bind("properties", propertiesComponent);
 		main.enableHangupSupport();
+		
 		main.addRouteBuilder(new HcSr505Route());
+		main.addRouteBuilder(new CoAPMockRoute());
 		try {
 			main.run();
 		} catch (Exception e) {
---
+++ bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/route/servo/ServoRoute.java
@@ -0,0 +1,13 @@
+package xyz.sergevas.iot.bcbot.robot.route.servo;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ServoRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
