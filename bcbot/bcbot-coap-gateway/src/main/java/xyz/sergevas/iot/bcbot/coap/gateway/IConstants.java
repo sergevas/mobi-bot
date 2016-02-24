6bf9d445f44fe438f24c696d95e4a1fb00b1ef10
Sergey Vasiliev <brighterr@gmail.com>
2016-02-24 03:19:40 +0300

    Development in progress.

--- bcbot/bcbot-coap-gateway/src/main/java/xyz/sergevas/iot/bcbot/coap/gateway/IConstants.java
+++ bcbot/bcbot-coap-gateway/src/main/java/xyz/sergevas/iot/bcbot/coap/gateway/IConstants.java
@@ -1,4 +1,15 @@
 package xyz.sergevas.iot.bcbot.coap.gateway;
 
 public interface IConstants {
+	
+	public static final String ROBOT_STOP_ROUTE = "direct:robot.stop";
+	public static final String ROBOT_MOVE_FORWARD_ROUTE = "direct:robot.move.forward";
+	public static final String ROBOT_MOVE_BACKWARD_ROUTE = "direct:robot.move.backward";
+	public static final String ROBOT_MOVE_TURN_RIGHT_ROUTE = "direct:robot.move.turn.right";
+	public static final String ROBOT_MOVE_TURN_LEFT_ROUTE = "direct:robot.move.turn.left";
+	
+	public static final String SERVO_SPEED_MODE_STOP = "STOP";
+	public static final String SERVO_SPEED_MODE_SLOW = "SLOW";
+	public static final String SERVO_SPEED_MODE_MEDIUM = "MEDIUM";
+	public static final String SERVO_SPEED_MODE_FAST = "FAST";
 }
--- bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/common/IConstants.java
+++ bcbot/bcbot-robot/src/main/java/xyz/sergevas/iot/bcbot/robot/common/IConstants.java
@@ -5,9 +5,9 @@ public interface IConstants {
 //	public static final String GPIO_PIN_HCSR505_PIN_LEFT = "0";
 //	public static final String GPIO_PIN_HCSR505_PIN_RIGHT = "2";
 	
+	public static final String ROBOT_STOP_ROUTE = "direct:robot.stop";
 	public static final String ROBOT_MOVE_FORWARD_ROUTE = "direct:robot.move.forward";
 	public static final String ROBOT_MOVE_BACKWARD_ROUTE = "direct:robot.move.backward";
-	public static final String ROBOT_STOP_ROUTE = "direct:robot.stop";
 	public static final String ROBOT_MOVE_TURN_RIGHT_ROUTE = "direct:robot.move.turn.right";
 	public static final String ROBOT_MOVE_TURN_LEFT_ROUTE = "direct:robot.move.turn.left";
 	
