8cf854ea1e1114a45699fe09508e6b3534198516
sergevas <brighterr@gmail.com>
2019-05-14 01:20:23 +0300

    Development in progress

--- src/main/java/xyz/sergevas/iot/mobibot/robot/bounadry/RobotStateLED.java
+++ src/main/java/xyz/sergevas/iot/mobibot/robot/bounadry/RobotStateLED.java
@@ -1,9 +1,26 @@
 package xyz.sergevas.iot.mobibot.robot.bounadry;
 
+import jdk.dio.DeviceConfig;
+import jdk.dio.gpio.GPIOPinConfig;
 import xyz.sergevas.iot.mobibot.robot.entity.Attenuator;
 import xyz.sergevas.iot.mobibot.robot.entity.Command;
 
 public class RobotStateLED implements Attenuator {
+	
+	private GPIOPinConfig ledPinConfig;
+	
+	
+
+	public RobotStateLED() {
+		this.ledPinConfig = new GPIOPinConfig.Builder()
+				.setControllerNumber(DeviceConfig.UNASSIGNED)
+				.setPinNumber(17)
+				.setDirection(GPIOPinConfig.DIR_OUTPUT_ONLY)
+				.setTrigger(GPIOPinConfig.TRIGGER_NONE)
+				.setDriveMode(GPIOPinConfig.MODE_OUTPUT_PUSH_PULL)
+				.setInitValue(false)
+				.build();
+	}
 
 	@Override
 	public <T extends Command> void execute(T t) {
